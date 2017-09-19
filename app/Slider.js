dc5f9bdcf7239a4d30466e3308acd576ec3a21f5
salwe <salwebreid@gmail.com>
2017-09-19 14:59:16 +0400

    Adding navigation

--- app/Slider.js
+++ app/Slider.js
@@ -2,24 +2,47 @@ class Slider {
     constructor(root) {
         this.root = root[0];
         this.sliders = [...root[0].children];
-        this.root.className += " sSlider";
-        
-        this.showSlide();
-        this.addArrows();
+
+        this.initialize();
     }
+    initialize() {
+        this.root.className += " sSlider";
+        this.activeSlider = 0;
+
+        this.addNavigation();
+        this.showSlide();
+    }
+
     showSlide(id = 0) {
         this.sliders.forEach(el => el.style.display = "none");
         this.sliders[id].style.display = "";
+        [...document.getElementsByClassName('paging')[0].children].forEach(el => el.classList.remove('active'));
+        document.getElementsByClassName('paging')[0].children[id].classList.add('active');
     }
-    addArrows() {
-        let arrowNext = document.createElement("div");
-        arrowNext.className = "arrow arrow-next";
-        arrowNext.innerHTML = '>';
-        this.root.append(arrowNext);
+    addNavigation() {
+        var nav = this.addElement('navigation', '');
+        var paging = this.addElement('paging', '');
+        this.addElement('arrow arrow-next', '>', nav, () => {
+            this.changeSlider((this.activeSlider < this.sliders.length - 1) ? (this.activeSlider + 1) : 0);
+        });
+        this.addElement('arrow arrow-prev', '<', nav, () => {
+            this.changeSlider((this.activeSlider > 0) ? (this.activeSlider - 1) : (this.sliders.length - 1));
+        });
 
-        let arrowPrev = document.createElement("div");
-        arrowPrev.className = "arrow arrow-prev";
-        arrowPrev.innerHTML = '<';
-        this.root.append(arrowPrev);
+        this.sliders.forEach((el, i) => { return this.addElement('circle', '', paging, () => { this.changeSlider(i) }); });
+    }
+
+    changeSlider(id) {
+        this.showSlide(id);
+        this.activeSlider = id;
+    }
+
+    addElement(className, text, parent = this.root, func) {
+        let element = document.createElement("div");
+        element.className = className;
+        element.innerHTML = text;
+        element.onclick = func;
+        parent.append(element);
+        return element;
     }
 }
